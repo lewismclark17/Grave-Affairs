1f778ed6894504bdba00fe3f0baef206062a0b61
lewismclark17 <68912515+lewismclark17@users.noreply.github.com>
2021-04-10 15:09:45 +0800

    Added Timer + Game Over Screen

---
+++ Grave Affairs/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString ("f0");
+        }
+        else
+        {
+           SceneManager.LoadScene("GameOver");
+        }
+    }
+}
+
