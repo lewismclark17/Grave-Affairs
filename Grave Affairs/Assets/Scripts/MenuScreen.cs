d14dda859a6c4bbb9932cd176d03dfffbebe5799
lewismclark17 <68912515+lewismclark17@users.noreply.github.com>
2021-04-22 20:50:57 +0800

    Added Main Menu + Proper Level Design

---
+++ Grave Affairs/Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreen : MonoBehaviour
+{
+    public GameObject NewGameButton;
+    public GameObject QuitButton;
+
+    public void Begin()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();     
+    }
+}
