79da07c0739e969217182abc0f8a14df0b994d8b
lewismclark17 <68912515+lewismclark17@users.noreply.github.com>
2021-09-05 17:01:18 +0800

    added complex corpse randomization system for cart delivery

--- Grave Affairs/Assets/Scripts/Cart.cs
+++ Grave Affairs/Assets/Scripts/Cart.cs
@@ -1,29 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Cart : MonoBehaviour
 {
-    public GameObject CartCorpses;
+    public GameObject cartCorpses;
 
-    public GameObject ActualCorpses;
+    public GameObject priestRef, soldierRef, plagueRef;
 
     public float sec = 5.5f;
+
+    public float rowSpacing, colSpacing, verSpacing;
+    
+    public int rowSize, colSize;
 
     void Start()
     {
         StartCoroutine(CorpseMagic());
+        GenerateCorpses(6, 8, 10, 12, 1, 3);
     }
 
     IEnumerator CorpseMagic()
     {
         yield return new WaitForSeconds(sec);
-        //CartCorpses.SetActive(false);
-        //ActualCorpses.SetActive(true);
-        /*foreach(Transform child in transform)
+        cartCorpses.transform.parent = null;
+    }
+
+    void GenerateCorpses(int minNumSoldiers, int maxNumSoldiers, int minNumPlagued, int maxNumPlagued, int minNumPriests, int maxNumPriests)
+    {
+        int numSoldiers = Random.Range(minNumSoldiers, maxNumSoldiers + 1);
+        int numPlagued = Random.Range(minNumPlagued, maxNumPlagued + 1);
+        int numPriests = Random.Range(minNumPriests, maxNumPriests + 1);
+
+        List<GameObject> corpses = new List<GameObject>();
+        for (int i = 0; i < numSoldiers; i++)
+        { 
+            corpses.Add(Instantiate(soldierRef, cartCorpses.transform));     
+        }
+        for (int i = 0; i < numPlagued; i++)
         {
-            child.parent = null;
-        }*/
-        CartCorpses.transform.parent = null;
+            corpses.Add(Instantiate(plagueRef, cartCorpses.transform));     
+        }
+        for (int i = 0; i < numPriests; i++)
+        {
+            corpses.Add(Instantiate(priestRef, cartCorpses.transform));     
+        }
+        corpses = corpses.OrderBy(a => Random.value).ToList();
+        for (int i = 0; i < corpses.Count; i++)
+        {
+            corpses[i].transform.localPosition = new Vector3(i%rowSize*rowSpacing, i/(rowSize*colSize)*verSpacing, i/rowSize%colSize*colSpacing);
+        }
     }
 }
