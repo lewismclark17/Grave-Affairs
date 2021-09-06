e94707a04532cafc964d5763633b7a7b63b4df24
Matthew-Grey <Matthewigrey@protonmail.com>
2021-09-06 13:53:31 +0800

    Added Sounds

---
+++ Grave Affairs/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE_LVL01 = 1266554271U;
+        static const AkUniqueID BOAT = 1795056175U;
+        static const AkUniqueID BODY_CLEAN = 3301051131U;
+        static const AkUniqueID CART = 4189504243U;
+        static const AkUniqueID FINAL_20 = 2339349540U;
+        static const AkUniqueID FURNACE_BURN = 2149484929U;
+        static const AkUniqueID MENU_CONFIRM = 4046753069U;
+        static const AkUniqueID MENU_MOVE = 1927068452U;
+        static const AkUniqueID PHYSICS_INTERACT = 3767330593U;
+        static const AkUniqueID TIME_UP = 4051042642U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TIME_REMAINING = 1094985903U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GA_TEST = 2087744648U;
+        static const AkUniqueID GA_TEST2 = 1829204522U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
